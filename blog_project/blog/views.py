2bb1efd6a79cd7e7beca0d1c4e577f93c4592d8a
Meagan Hollingsworth <mhollingsworth@google.com>
2023-11-17 20:51:02 -0500

    deleted dummy dated and updated data through the database

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,34 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'Author': 'UserOne',
-        'Title': 'Blog Post 1',
-        'Content': 'First post content',
-        'Genre': 'Hip-hop',
-        'Date_posted': 'November 16, 2023'
-    },
-    {
-        'Author': 'UserTwo',
-        'Title': 'Blog Post 2',
-        'Content': 'Second post content',
-        'Genre': 'R&B',
-        'Date_posted': 'November 17, 2023'
-    },
-    {
-        'Author': 'UserThree',
-        'Title': 'Blog Post 3',
-        'Content': 'Third post content',
-        'Genre': 'Pop',
-        'Date_posted': 'October 31, 2023'
-    }
-]
+
 
 # Create your views here.
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, "blog/home.html", context)
 
