5a63791a46f13c234f10fba3b99446b251cb834c
Meagan Hollingsworth <mhollingsworth@google.com>
2023-11-21 14:51:56 -0500

    created the login and logout paths, also linked them to the home and register pages, And changed the profile name to users_profile

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
+LOGIN_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
