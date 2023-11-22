e163933cc6f7219b1733c7c06bb571014eb2abbc
Meagan Hollingsworth <mhollingsworth@google.com>
2023-11-22 18:26:01 -0500

    intergrated djang RESTful API so i can add a react frontend framework

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from rest_framework.routers import DefaultRouter
+from .api_views import PostViewSet
+
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
 
 
 
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('', PostListView.as_view(), name='home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
