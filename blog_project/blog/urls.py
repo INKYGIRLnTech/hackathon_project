65c1f6299a3cc1baaca9b973674186fb4ca38a09
Meagan Hollingsworth <mhollingsworth@google.com>
2023-11-27 12:07:25 -0500

    decided not to go with material ui and reimported bootstrap

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path, include
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-from rest_framework.routers import DefaultRouter
-from .api_views import PostViewSet
 
 
-router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
+
+
+
 
 
 
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    
     path('', PostListView.as_view(), name='home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -12,8 +12,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'blog/static'),
+]
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,8 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'corsheaders',
+    'bootstrap5',
+   
 ]
 
 MIDDLEWARE = [
@@ -51,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -87,7 +94,7 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -128,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'home'
