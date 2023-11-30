94979fe9a58d7a0ba66bf6789ae63ae4a8e36b0a
Meagan Hollingsworth <mhollingsworth@google.com>
2023-11-30 15:08:38 -0500

    fixed the sizing of the images in of the user's profile

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -16,7 +16,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blog/static'),
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
